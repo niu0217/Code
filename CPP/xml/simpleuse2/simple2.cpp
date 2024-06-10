226d5f6a762511978217fee6f70c6a29035b9024
niu0217 <1033856427@qq.com>
2024-06-10 20:45:05 +0800

    完成xml的基础文件解析

---
+++ CPP/xml/simpleuse2/simple2.cpp
@@ -0,0 +1,23 @@
+#include "tinyxml2.h"
+#include <iostream>
+
+using namespace std;
+using namespace tinyxml2;
+int main()
+{
+  XMLDocument xml;
+  xml.LoadFile("test.xml");
+
+  XMLElement *category = xml.RootElement();
+  XMLElement *book = category->FirstChildElement("book");
+  XMLElement *number = category->FirstChildElement("number");
+  XMLElement *animal = category->FirstChildElement("animal");
+  const char *book_name = book->GetText();
+  cout << book_name << endl;
+  int num = atoi(number->GetText());
+  cout << num << endl;
+  const char *animal_name = animal->GetText();
+  cout << animal_name << endl;
+
+  return 0;
+}
